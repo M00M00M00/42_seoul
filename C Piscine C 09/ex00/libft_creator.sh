01b69fd85550d4ad41edc1228555717e4802421e
Museung Kim <mu3980@gmail.com>
2022-02-19 11:12:48 +0900

    commit from cluster

--- C Piscine C 09/ex00/libft_creator.sh
+++ C Piscine C 09/ex00/libft_creator.sh
@@ -1,3 +1,15 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    libft_creator.sh                                   :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: mukim <mukim@student.42.fr>                +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/02/19 10:54:13 by mukim             #+#    #+#              #
+#    Updated: 2022/02/19 10:54:14 by mukim            ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 gcc -Wall -Wextra -Werror -c ft_putchar.c ft_swap.c ft_putstr.c ft_strlen.c ft_strcmp.c
 ar rc libft.a ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o
 rm *.o
