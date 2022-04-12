489288ab964503b138850bbe0c9dfe29bcb4f347
Museung Kim <mu3980@gmail.com>
2022-04-12 13:04:51 +0900

    Revert "commit on 07/04/2022"
    This reverts commit da887622466748b0870eb0052f5dc36a7a7a9f3d.

---
+++ Libft/ft_lstiter.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mukim <mukim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/05 18:49:18 by mukim             #+#    #+#             */
+/*   Updated: 2022/04/05 19:16:31 by mukim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	t_list	*temp;
+
+	temp = lst;
+	if (!lst || !f)
+		return ;
+	while (temp)
+	{
+		f(temp->content);
+		temp = temp->next;
+	}
+}
